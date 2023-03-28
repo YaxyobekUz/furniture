a41358737d59ead6a69889de692494cbfa0f8d66
mryaxyobek <pubgn9642@gmail.com>
2023-03-28 14:04:04 +0500

    added global styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        br:{
+          'primary-color':"#054C73",
+          'primary-dark':"#054C73",
+          'font-color':"#333333",
+          'dark-color':"#111111",
+          'light-dark-color':"#666666",
+          'light-blue':"#F2F5FF",
+          'hero-light-blue':"#DFE9F4"
+        }
+      },
+    },
   },
   plugins: [],
 }
